db6e72cf9a2b1d9110c54578faad52d18ece3207
BalaMunde123 <balamunde123@gmail.com>
2022-11-06 11:47:19 +0530

    Added fist git priject

---
+++ FistGitProject1/src/main/java/com/example/demo/FistGitProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FistGitProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FistGitProject1Application.class, args);
+	}
+
+}
